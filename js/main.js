2bd57849dd52bfaa4aaecc25473ec6edb7cbed8e
obsu kebede <134233644+ob971@users.noreply.github.com>
2024-01-04 11:19:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,12 +34,6 @@
             $('.back-to-top').fadeOut('slow');
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
 
     
 })(jQuery);
